ed3a0f33749dad7715de3dd9bc4183bf9ecd0786
Dhruv Totala <dhruvtotla28@gmail.com>
2025-01-28 00:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (65.93%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-    vector <int> ans;
-    int rem = 1;
-    for(int i = digits.size() - 1; i >= 0; i--){
-        int sum = digits[i] + rem;
-        rem = sum / 10;
-        ans.push_back(sum % 10);
-    }   
-    if(rem) ans.push_back(rem); 
-    reverse(ans.begin(), ans.end());
-    return ans;
+        int idx = digits.size() - 1; // Start from the last digit
+        while (idx >= 0) {
+            if (digits[idx] == 9) {
+                digits[idx] = 0; // Set current digit to 0 if it's 9
+            } else {
+                digits[idx] += 1; // Otherwise, just add 1
+                return digits; // Return the result immediately
+            }
+            idx--; // Move to the next digit
+        }
+        digits.insert(digits.begin(), 1); // If all digits were 9, add a leading 1
+        return digits;
     }
 };
