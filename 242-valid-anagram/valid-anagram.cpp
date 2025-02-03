37b4bc64c56135fa2957aadda10693daf0f91705
Dhruv Totala <dhruvtotla28@gmail.com>
2025-02-04 01:59:31 +0530

    Time: 1 ms (64.98%) | Memory: 9.9 MB (26.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,10 +3,12 @@ public:
     bool isAnagram(string s, string t) {
         unordered_map <char ,int> mp;
         if(t.size() != s.size()) return false;
-        for(int i = 0; i < s.size(); i++) mp[s[i]]++;
+        for(int i = 0; i < s.size(); i++){
+        mp[s[i]]++;
+        mp[t[i]]--;   
+        } 
         for(int i = 0; i < t.size(); i++){
-           if(mp[t[i]] == 0) return false;
-           mp[t[i]] --; 
+           if(mp[t[i]] != 0) return false;
         }
         return true;
     }
