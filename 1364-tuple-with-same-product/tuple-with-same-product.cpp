122878cf9063febf25d2f53d3ffa6d28e359285d
Dhruv Totala <dhruvtotla28@gmail.com>
2025-02-06 13:58:41 +0530

    Time: 334 ms (36.68%) | Memory: 85.1 MB (45.45%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -4,18 +4,17 @@ public:
     int n = nums.size();
     int ans = 0;
     sort(nums.begin(), nums.end());
+    unordered_map <int, int> mp;
     for(int i = 0; i < n; i++){
-        for(int j = n - 1; j > i; j--){
-            int prod = nums[i] * nums[j];
-            unordered_set <int> st;
-            for(int k = i + 1; k < j; k++){
-                if(prod % nums[k] == 0){
-                    int dval = prod / nums[k];
-                    if(st.count(dval)) ans++;
-                    st.insert(nums[k]);
-                }
-            }
-        }
+    for(int j = i + 1; j < n; j++){
+        int prod = nums[i] * nums[j];
+        mp[prod]++;
+    }
+    }
+    for(auto it : mp){
+        int num = it.first;
+        int freq = it.second;
+        ans += (freq * (freq - 1)) / 2;
     }
     return ans * 8;
     }
