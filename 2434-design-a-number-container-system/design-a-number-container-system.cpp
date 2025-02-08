35ab21cb753f45780ede601753c951c5a6759193
Dhruv Totala <dhruvtotla28@gmail.com>
2025-02-09 02:20:29 +0530

    Time: 152 ms (12.84%) | Memory: 197.9 MB (8.96%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,31 +1,26 @@
-#include <map>
-#include <set>
-
 class NumberContainers {
 public:
-    std::map<int, int> mp;              // Maps index -> number
-    std::map<int, std::set<int>> mp1;   // Maps number -> sorted indices
+    map<int, int> mp;
+    map<int, set<int>> mp1;   
 
     NumberContainers() {}
 
     void change(int index, int number) {
-        // If index already has a number, remove it from mp1 (reverse mapping)
         if (mp.count(index)) {
             int prevnum = mp[index];
             mp1[prevnum].erase(index);
             if (mp1[prevnum].empty()) {
-                mp1.erase(prevnum);  // Clean up empty sets
+                mp1.erase(prevnum); 
             }
         }
-        // Update mappings
         mp[index] = number;
         mp1[number].insert(index);
     }
 
     int find(int number) {
         if (mp1.count(number) && !mp1[number].empty()) { 
-            return *mp1[number].begin(); // Return the smallest index
+            return *mp1[number].begin();
         }
-        return -1; // If number is not found
+        return -1;
     }
 };
