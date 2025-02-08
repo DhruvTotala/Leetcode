70960a25b4200dbc8280c356a6a8785c5893b13e
Dhruv Totala <dhruvtotla28@gmail.com>
2025-02-09 02:21:47 +0530

    Time: 201 ms (5.37%) | Memory: 198 MB (6.57%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -2,21 +2,16 @@ class NumberContainers {
 public:
     map<int, int> mp;
     map<int, set<int>> mp1;   
-
     NumberContainers() {}
-
     void change(int index, int number) {
         if (mp.count(index)) {
             int prevnum = mp[index];
             mp1[prevnum].erase(index);
-            if (mp1[prevnum].empty()) {
-                mp1.erase(prevnum); 
-            }
+            if (mp1[prevnum].empty()) mp1.erase(prevnum);   
         }
         mp[index] = number;
         mp1[number].insert(index);
     }
-
     int find(int number) {
         if (mp1.count(number) && !mp1[number].empty()) { 
             return *mp1[number].begin();
