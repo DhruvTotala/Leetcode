85e7609a90253f38bca9aecaefe3d64343814522
Dhruv Totala <dhruvtotla28@gmail.com>
2025-02-27 01:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.99%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
     int r = n - 1;
     while(l < r){
         int mid = (l + r) / 2;
-        if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) l = mid + 1;
+        if(nums[mid] == nums[(mid ^ 1)]) l = mid + 1;
         else r = mid;
     } 
     return nums[l];
