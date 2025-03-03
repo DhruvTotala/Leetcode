0b8440f96adba026bf5c2dc5ea38927c8ac6f711
Dhruv Totala <dhruvtotla28@gmail.com>
2025-03-03 14:53:10 +0530

    Time: 11 ms (31.49%) | Memory: 127.7 MB (87.50%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-    int n = nums.size();
-    vector <int> ans;
-    for(int i = 0; i < n; i++){
-        if(nums[i] < pivot) ans.push_back(nums[i]);
-    }   
-    for(int i = 0; i < n; i++){
-        if(nums[i] == pivot) ans.push_back(nums[i]);
-    }
-     for(int i = 0; i < n; i++){
-        if(nums[i] > pivot) ans.push_back(nums[i]);
-    }
-    return ans;
+        int n = nums.size();
+        vector<int> result(n);
+        int left = 0, right = n - 1, mid = 0;
+
+        // First pass: count smaller elements
+        for (int num : nums)
+            if (num < pivot) result[left++] = num;
+
+        // Second pass: place pivot elements
+        mid = left;
+        for (int num : nums)
+            if (num == pivot) result[mid++] = num;
+
+        // Third pass: place greater elements
+        for (int num : nums)
+            if (num > pivot) result[mid++] = num;
+
+        return result;
     }
 };
