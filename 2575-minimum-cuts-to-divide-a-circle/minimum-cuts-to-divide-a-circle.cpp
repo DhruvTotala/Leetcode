41c721bf76934c8bed9a072c3892e9fe1877884c
Dhruv Totala <dhruvtotla28@gmail.com>
2025-03-05 14:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (82.64%) - LeetSync

--- 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.cpp
+++ 2575-minimum-cuts-to-divide-a-circle/minimum-cuts-to-divide-a-circle.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-    int numberOfCuts(int n) {
-        int ans = 0;
-        if(n == 1) return ans;
-        if(n % 2 == 0) ans = n / 2;
-        else ans = n;
-        return ans;
+    int numberOfCuts(int n) 
+    {
+        return (n > 1 and n % 2) ? n : n / 2;    
     }
 };
