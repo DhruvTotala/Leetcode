84929340366ee929f53222adf678850688bfb5be
Dhruv Totala <dhruvtotla28@gmail.com>
2025-03-05 14:29:56 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.17%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans = 1;
-        n = n - 1;
-        if(n == 0) return ans;
-        for(int i = 1; i <= n; i++){
-            ans = ans + (4 * i); 
-        }
-        return ans;
+    return 1 + (long long)n * (n - 1) * 2;
     }
 };
