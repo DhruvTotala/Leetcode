007e2dbdcfc65345ac6d211cff1c58af3f8196ab
Dhruv Totala <dhruvtotla28@gmail.com>
2025-03-06 07:48:03 +0530

    Time: 25 ms (10.25%) | Memory: 32.6 MB (5.79%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -7,14 +7,12 @@ public:
         for(int num : it) mp[num] ++;
     }
     vector <int> ans;
-    int r = -1;
-    int m = -1;
     for(int i = 1; i <= n * n; i++){
-        if(mp[i] == 2) r = i;
-        if(mp[i] == 0) m = i;
+        if(mp[i] == 2) ans.push_back(i);
     }
-    ans.push_back(r);
-    ans.push_back(m);
+    for(int i = 1; i <= n * n; i++){
+        if(mp[i] == 0) ans.push_back(i);
+    }
     return ans;
     }
 };
