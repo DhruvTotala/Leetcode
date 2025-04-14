4d34fc6ce7d666794c3fe5752b3e5ce1ed26aca6
Dhruv Totala <dhruvtotla28@gmail.com>
2025-04-14 13:08:01 +0530

    Time: 6 ms (78.59%) | Memory: 11 MB (71.62%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -1,15 +1,16 @@
 class Solution {
-public:
-    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int n = arr.size();
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                for(int k = j + 1; k < n; k++) {
-                    if((abs(arr[i] - arr[j]) <= a) && (abs(arr[j] - arr[k]) <= b) && (abs(arr[k] - arr[i]) <= c) ) count ++;
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            int good = 0;
+            for (int i = 0 ; i < arr.size() - 2 ; i++) {
+                for (int k = i + 2 ; k < arr.size() ; k++) {
+                    if (abs(arr[k] - arr[i]) > c) continue;
+                    for (int j = i + 1; j < k ; j++) 
+                    if (abs(arr[i] - arr[j]) <= a && 
+                        abs(arr[j] - arr[k]) <= b) 
+                        ++good; 
                 }
             }
-        }
-        return count;
+        return good;
     }
 };
