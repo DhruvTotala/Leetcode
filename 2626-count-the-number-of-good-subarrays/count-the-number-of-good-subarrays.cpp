d5ab4d214316b5df53bdf28344dc630a9305473a
Dhruv Totala <dhruvtotla28@gmail.com>
2025-04-16 05:33:50 +0530

    Time: 84 ms (49.05%) | Memory: 79.2 MB (36.26%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     long long countGood(vector<int>& nums, int k) {
-        int n = nums.size();
-        int same = 0, right = -1;
-        unordered_map<int, int> cnt;
-        long long ans = 0;
-        for (int left = 0; left < n; ++left) {
-            while (same < k && right + 1 < n) {
-                ++right;
-                same += cnt[nums[right]];
-                ++cnt[nums[right]];
+        long long res = 0;
+        unordered_map<int, int> freq;
+        int left = 0;
+        long long count = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            count += freq[nums[right]];
+            freq[nums[right]]++;
+
+            while (count >= k) {
+                res += nums.size() - right;
+                freq[nums[left]]--;
+                count -= freq[nums[left]];
+                left++;
             }
-            if (same >= k) {
-                ans += n - right;
-            }
-            --cnt[nums[left]];
-            same -= cnt[nums[left]];
         }
-        return ans;
+
+        return res;
     }
 };
