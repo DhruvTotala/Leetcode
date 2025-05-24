49b9fbc83291c24ddfd826331b3617a2d4cb1734
Dhruv Totala <dhruvtotla28@gmail.com>
2025-05-24 12:26:07 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (28.80%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -5,11 +5,11 @@ public:
         for(int i = 0; i < words.size(); i++) {
             for(char ch : words[i]) {
                 if(ch == x) {
-                ans.push_back(i);
-                break;
+                    ans.push_back(i);
+                    break;
+                }
             }
         }
-    }
         return ans;
     }
 };
