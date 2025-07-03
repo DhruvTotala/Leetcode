75d0be173e7fde0454d9253c55c8b7f39efa52a5
Dhruv Totala <dhruvtotla28@gmail.com>
2025-07-04 00:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (100.00%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        std::string sb = "a";
-        while (sb.length() < k) {
-            int size = sb.length();
-            for (int i = 0; i < size; ++i) {
-                char nextChar = 'a' + ((sb[i] - 'a' + 1) % 26);
-                sb += nextChar;
-            }
-        }
-        return sb[k - 1];
+        int shift = __builtin_popcount(k - 1);
+        return 'a' + shift;
     }
 };
