7270b933be16f20b9a7cabf23de349f42791a0be
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-02 20:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.02%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map <int, int> mp;
         int n = nums.size();
-        int total = n * (n + 1) / 2;
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += nums
-            [i];
+        int nums_xor = nums[0];
+        for(int i = 1; i < n; i++) {
+            nums_xor ^= nums[i];
         }
-        return total - sum;
+        int size_xor = 0;
+        for(int i = 1; i <= n; i++) {
+            size_xor ^= i;
+        }
+        return size_xor ^ nums_xor;
     }
 };
