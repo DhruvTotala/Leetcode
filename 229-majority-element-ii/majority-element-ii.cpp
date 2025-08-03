6e811088fe204980bfa37cbfe929fe2940ccd317
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-03 20:05:59 +0530

    Time: 11 ms (23.76%) | Memory: 26.4 MB (28.58%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> result;
+        unordered_map <int, int> mp;
+        for(auto it : nums) mp[it]++;
+        for(auto it : mp) {
+            if(it.second > (n / 3)) result.push_back(it.first);
+        }
+        return result;
+    }
+};
