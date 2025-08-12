0a74fa91b99f5c585a97e297a7b5e91db1cb64ac
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-12 11:14:21 +0530

    Time: 15 ms (21.52%) | Memory: 23.1 MB (7.53%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool caneat(vector<int>& piles, int h, int speed) {
+        long long hours = 0;
+        for (int pile : piles) {
+            hours += ceil((double)pile / speed); // use ceil function
+        }
+        return hours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (caneat(piles, h, mid)) {
+                high = mid - 1; // try smaller speed
+            } else {
+                low = mid + 1; // need faster speed
+            }
+        }
+        return low;
+    }
+};
