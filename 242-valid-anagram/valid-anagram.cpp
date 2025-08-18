c09e087079c6463575c18228aa9d25644a05844a
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-19 02:12:05 +0530

    Time: 3 ms (51.85%) | Memory: 9.7 MB (48.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map <char, int> mp;
-        for(char ch : s) mp[ch]++;
-        if(s.size() != t.size()) return false;
-        for(int i = 0; i < t.size(); i++) {
-            if(mp.find(t[i]) == mp.end() || mp[t[i]] == 0) return false;
-            else mp[t[i]]--;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
