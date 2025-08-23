a98b414f015e66eaaf2e0ba1db4709d4eb761f75
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-23 22:21:10 +0530

    Time: 4 ms (18.29%) | Memory: 14.5 MB (14.97%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     void recur(int idx, vector<int>& nums, vector<int>& temp, set<vector<int>>& st) {
         if(idx == nums.size()) {
-            st.insert(temp);  // directly insert
+            st.insert(temp);  
             return;
         }
+        recur(idx + 1, nums, temp, st); // not take
 
-        // not take
-        recur(idx + 1, nums, temp, st);
-
-        // take
-        temp.push_back(nums[idx]);
+        temp.push_back(nums[idx]); // take
         recur(idx + 1, nums, temp, st);
         temp.pop_back();
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); // important for duplicates
+        sort(nums.begin(), nums.end());
         set<vector<int>> st;
         vector<int> temp;
         recur(0, nums, temp, st);
-
-        // convert set to vector
         return vector<vector<int>>(st.begin(), st.end());
     }
 };
