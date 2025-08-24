dd2f581b374c90ab772efd5538823b9d1f9821a3
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-24 15:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (61.67%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        auto ub = upper_bound(letters.begin(), letters.end(), target);
-        if (ub == letters.end()) return letters[0];
-        return *ub; 
+        int n = letters.size();
+        int ub = upper_bound(letters.begin(), letters.end(), target) - letters.begin();
+        if (ub == n) return letters[0];
+        return letters[ub];
     }
 };
