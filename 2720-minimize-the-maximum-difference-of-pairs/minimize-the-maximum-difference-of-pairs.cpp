311625a9a592e23c2b1d6babbf5e4eb45a68aeb3
Dhruv Totala <dhruvtotla28@gmail.com>
2025-08-27 01:31:46 +0530

    Time: 21 ms (84.57%) | Memory: 86.7 MB (82.90%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    int countPairs(vector<int>& nums, int maxDiff) {
+
+    bool ispossible(vector <int>& nums, int mid, int p) {
         int count = 0;
         int i = 0;
-        while (i < nums.size() - 1) {
-            if (nums[i + 1] - nums[i] <= maxDiff) {
+        while(i < nums.size() - 1) {
+            if(abs(nums[i] - nums[i + 1]) <= mid) {
                 count++;
-                i += 2; // pair formed, skip next
+                i += 2;
             } else {
                 i++;
             }
+            if(count >= p) return true;
         }
-        return count;
+        return false;
     }
 
     int minimizeMax(vector<int>& nums, int p) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int low = 0, high = nums.back() - nums.front();
+        int low = 0;
+        int high = nums[n - 1] - nums[0];
         int ans = high;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (countPairs(nums, mid) >= p) {
-                ans = mid;
-                high = mid - 1; // try smaller maxDiff
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(ispossible(nums, mid, p)) {
+               ans = mid;
+               high = mid - 1;
             } else {
-                low = mid + 1; // need a larger maxDiff
+                low = mid + 1;
             }
         }
-
         return ans;
     }
 };
