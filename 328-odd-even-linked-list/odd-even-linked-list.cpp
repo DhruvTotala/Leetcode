e14ea50502af2a6077596578be4a91330d0abd63
Dhruv Totala <dhruvtotla28@gmail.com>
2025-09-11 15:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (88.10%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (!head) return nullptr;
 
         ListNode* curr = head;
         ListNode* headA = new ListNode();  // dummy for odd
@@ -12,23 +11,23 @@ public:
         int count = 1;
         while (curr != NULL) {
             ListNode* temp = curr;
-            curr = curr->next;
-            temp->next = NULL; // disconnect node from original chain
+            curr = curr -> next;
+            temp -> next = NULL; // disconnect node from original chain
             
             if (count % 2 == 1) {  // odd
-                p1->next = temp;
-                p1 = p1->next;
+                p1 -> next = temp;
+                p1 = p1 -> next;
             } else {               // even
-                p2->next = temp;
-                p2 = p2->next;
+                p2 -> next = temp;
+                p2 = p2 -> next;
             }
             count++;
         }
         
         // connect odd list with even list
-        p1->next = headB->next;
-        p2->next = NULL; // terminate even list
+        p1 -> next = headB -> next;
+        p2 -> next = NULL; // terminate even list
         
-        return headA->next; // skip dummy
+        return headA -> next; // skip dummy
     }
 };
