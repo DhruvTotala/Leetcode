0e3321643f5871df25295f664718e6bc7b239e51
Dhruv Totala <dhruvtotla28@gmail.com>
2025-09-11 14:14:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (25.54%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,24 +10,13 @@
  */
 class Solution {
 public:
-    int len_ll(ListNode* head) {
-        int count = 0;
-        ListNode* curr = head;
-        while(curr != NULL) {
-            count++;
-            curr = curr -> next;
-        }
-        return count;
-    }
-
     ListNode* middleNode(ListNode* head) {
-        int len = len_ll(head);
-        int mid = (len / 2) + 1;
-        ListNode* curr = head;
-        while(mid > 1) {
-            mid--;
-            curr = curr -> next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        return curr;
+        return slow;
     }
 };
