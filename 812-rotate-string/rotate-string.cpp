c5cecab2885fad1e7971d635d9947a2547f8ec06
Dhruv Totala <dhruvtotla28@gmail.com>
2025-09-22 03:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.93%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        return ((goal + goal).find(s) != string::npos);
+    }
+};
