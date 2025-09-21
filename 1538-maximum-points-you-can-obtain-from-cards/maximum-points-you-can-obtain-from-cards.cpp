d9a1dfae0689bdfdaeb3a54031a1c51f59107292
Dhruv Totala <dhruvtotla28@gmail.com>
2025-09-21 23:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (79.71%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
-        int total = accumulate(cardPoints.begin(), cardPoints.end(), 0);
-
-        // If we take all cards
-        if (k == n) return total;
-
-        int window = n - k; // size of subarray to exclude
-        int curr_sum = 0;
-
-        // First window
-        for (int i = 0; i < window; i++) {
-            curr_sum += cardPoints[i];
+        int max_sum = 0;
+        int l_sum = 0;
+        int r_sum = 0;
+        for(int i = 0; i < k; i++) l_sum += cardPoints[i];
+        max_sum = l_sum;
+        int r_index = n - 1;
+        for(int i = k - 1; i >= 0; i--) {
+            l_sum = l_sum - cardPoints[i];
+            r_sum = r_sum + cardPoints[r_index];
+            r_index--;
+            max_sum = max(max_sum, l_sum + r_sum);
         }
-
-        int min_sum = curr_sum;
-
-        // Sliding window
-        for (int i = window; i < n; i++) {
-            curr_sum += cardPoints[i] - cardPoints[i - window];
-            min_sum = min(min_sum, curr_sum);
-        }
-
-        return total - min_sum;
+        return max_sum;
     }
 };
