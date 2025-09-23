d5aa61404c9e2326e3c5bc89e03e523d4c892bd0
Dhruv Totala <dhruvtotla28@gmail.com>
2025-09-23 17:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (30.92%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    static int compareVersion(string& v1, string& v2) {
-        const int n1=v1.size(), n2=v2.size();
-        int x1=0, x2=0;
-        for(int i=0, j=0; i<n1 || j<n2; i++, j++){
-            while(i<n1 && v1[i]!='.'){
-                x1=10*x1+(v1[i++]-'0');
-            }
-            while(j<n2 && v2[j]!='.'){
-                x2=10*x2+(v2[j++]-'0');
-            }
-            if (x1<x2) return -1;
-            else if (x1>x2) return 1;
-            x1=0;
-            x2=0;
+    int compareVersion(string version1, string version2) {
+        stringstream s1(version1), s2(version2);
+        string token1, token2;
+        
+        while (!s1.eof() || !s2.eof()) {
+            token1.clear();
+            token2.clear();
+            
+            if (!getline(s1, token1, '.')) token1 = "0";
+            if (!getline(s2, token2, '.')) token2 = "0";
+            
+            int num1 = stoi(token1);
+            int num2 = stoi(token2);
+            
+            if (num1 < num2) return -1;
+            if (num1 > num2) return 1;
         }
         return 0;
     }
