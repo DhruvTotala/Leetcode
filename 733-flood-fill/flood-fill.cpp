30801037fc652d8eb4e09b2595389d48ff15c6b7
Dhruv Totala <dhruvtotla28@gmail.com>
2025-10-19 17:41:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (46.04%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>>& ans, vector<vector<int>>& image, int newcolor, int delrow[], int delcol[], int inicolor) {
-        ans[row][col] = newcolor;
-        int n = image.size();
-        int m = image[0].size();
-        for(int i = 0; i < 4; i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == inicolor && ans[nrow][ncol] != newcolor) {
-                dfs(nrow, ncol, ans, image, newcolor, delrow, delcol, inicolor);
-            }
-        }
-    }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int inicolor = image[sr][sc];
-        vector<vector<int>>& ans = image;
+        if (image[sr][sc] == color) return image;
+        int n = image.size();
+        int m = image[0].size();
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1};
-        dfs(sr, sc, ans, image, color, delrow, delcol, inicolor);
-        return ans;
+        image[sr][sc] = color;
+
+        while(!q.empty()){
+            auto [row, col] = q.front();
+            q.pop();
+
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == inicolor) {
+                    q.push({nrow, ncol});
+                    image[nrow][ncol] = color;
+                }
+            }
+        }
+        return image;
     }
 };
