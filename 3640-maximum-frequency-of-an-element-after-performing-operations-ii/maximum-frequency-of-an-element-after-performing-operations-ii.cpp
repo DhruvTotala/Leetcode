5012e1afc749cdde592c81c51cb55acb595df3d6
Dhruv Totala <dhruvtotla28@gmail.com>
2025-10-22 13:32:22 +0530

    Time: 847 ms (54.98%) | Memory: 213.1 MB (53.08%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -1,23 +1,74 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        map<int,int>mp;
-        unordered_map<int,int>hash;
-        set<int>points;
-        for(auto &it:nums){
-            hash[it]++;
-            mp[it-k]++;
-            mp[it+k+1]--;
-            points.insert(it);
-            points.insert(it-k);
-            points.insert(it+k+1);
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        unordered_map<int, int> numCount;
+        set<int> modes;
+
+        auto addMode = [&](int value) {
+            modes.insert(value);
+            if (value - k >= nums.front()) {
+                modes.insert(value - k);
+            }
+            if (value + k <= nums.back()) {
+                modes.insert(value + k);
+            }
+        };
+
+        int lastNumIndex = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[lastNumIndex]) {
+                numCount[nums[lastNumIndex]] = i - lastNumIndex;
+                ans = max(ans, i - lastNumIndex);
+                addMode(nums[lastNumIndex]);
+                lastNumIndex = i;
+            }
         }
-        int ans=1;
-        int sum=0;
-        for(auto &it:points){
-            sum+=mp[it];
-            ans=max(ans,hash[it]+min(sum-hash[it],numOperations));
+
+        numCount[nums[lastNumIndex]] = nums.size() - lastNumIndex;
+        ans = max(ans, (int)nums.size() - lastNumIndex);
+        addMode(nums[lastNumIndex]);
+
+        auto leftBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] < value) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        };
+
+        auto rightBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right + 1) / 2;
+                if (nums[mid] > value) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            }
+            return left;
+        };
+
+        for (int mode : modes) {
+            int l = leftBound(mode - k);
+            int r = rightBound(mode + k);
+
+            int tempAns;
+            if (numCount.count(mode)) {
+                tempAns = min(r - l + 1, numCount[mode] + numOperations);
+            } else {
+                tempAns = min(r - l + 1, numOperations);
+            }
+            ans = max(ans, tempAns);
         }
+
         return ans;
     }
 };
