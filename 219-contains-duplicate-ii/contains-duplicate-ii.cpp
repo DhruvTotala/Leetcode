1947dad3ce9a3da2d39c36478157bfd99277f91d
Dhruv Totala <dhruvtotla28@gmail.com>
2025-11-02 14:33:30 +0530

    Time: 65 ms (93.69%) | Memory: 98.8 MB (33.88%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -5,9 +5,9 @@ public:
         unordered_map <int, int> mp;
         for(int i = 0; i < n; i++) {
             if(mp.find(nums[i]) != mp.end()) {
-               if(abs(i - mp[nums[i]]) <= k) return true;
+                if(abs(i - mp[nums[i]]) <= k) return true;
             }
-            mp[nums[i]] = i;
+        mp[nums[i]] = i;
         }
         return false;
     }
