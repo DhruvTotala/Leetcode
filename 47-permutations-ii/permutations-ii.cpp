ad5c982ff7e85461042c32fcbd8c71af74549882
Dhruv Totala <dhruvtotla28@gmail.com>
2026-01-18 01:57:08 +0530

    Time: 8 ms (29.78%) | Memory: 15.7 MB (10.78%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -24,7 +24,7 @@ public:
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
         permute(n, 0, nums);
         return res;
     }
