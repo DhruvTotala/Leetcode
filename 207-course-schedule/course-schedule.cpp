90137f0c49372a72e1766ce07ee36222a5cbef98
Dhruv Totala <dhruvtotla28@gmail.com>
2026-03-08 21:42:50 +0530

    Time: 7 ms (47.97%) | Memory: 19.9 MB (41.72%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    bool topology(unordered_map <int, vector<int>>& adj, vector <int>& indegree, int n) {
-        int count = 0;
-        queue <int> q;
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-                count++;
-            }
+    bool iscycledfs(unordered_map <int, vector<int>>& adj, int node, vector <bool>& 
+    visited, vector <bool>& inrecur) {
+        visited[node] = true;
+        inrecur[node] = true;
+        for(auto &it : adj[node]) {
+            if(!visited[it] && iscycledfs(adj, it, visited, inrecur)) return true;
+            else if(inrecur[it] == true) return true;
         }
-
-        while(!q.empty()) {
-            int f = q.front();
-            q.pop();
-            for(auto it : adj[f]) {
-                indegree[it]--;
-                if(indegree[it] == 0) {
-                    count++;
-                    q.push(it);
-                }
-            }
-        }
-        if(count == n) return true;
+        inrecur[node] = false;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map <int, vector<int>> adj;
-        vector <int> indegree(numCourses, 0);
         for(vector <int>& v : prerequisites) {
             int a = v[0];
             int b = v[1];
             adj[b].push_back(a);
-            indegree[a]++;
         }
-        return topology(adj, indegree, numCourses);
+        vector <bool> visited(numCourses, false);
+        vector <bool> inrecur(numCourses, 0);
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i] && iscycledfs(adj, i, visited, inrecur)) return false;
+        }
+        return true;
     }
 };
