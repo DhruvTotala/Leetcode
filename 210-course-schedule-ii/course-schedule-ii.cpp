b8bd08c5a5ee4fb65813910509ea14b04a23284e
Dhruv Totala <dhruvtotla28@gmail.com>
2026-03-10 00:39:46 +0530

    Time: 7 ms (34.77%) | Memory: 19 MB (30.52%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    vector <int> topo(unordered_map <int, vector<int>>& mp, int n, vector <int>& indegree) {
-        queue <int> q;
-        vector <int> result;
-        int count = 0;
-        for(int i = 0; i < n; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-                count++;
+    bool iscycledfs;
+    void dfs(unordered_map <int, vector<int>>& mp, int node, vector <bool>& visited, 
+    vector <bool>& inrecursion, stack <int>& st) {
+        visited[node] = true;
+        inrecursion[node] = true;
+        for(auto &it : mp[node]) {
+            if(inrecursion[it] == true) {
+                iscycledfs = true;
+                return;
             }
+            if(!visited[it]) dfs(mp, it, visited, inrecursion, st);
         }
-        while(!q.empty()) {
-            int f = q.front();
-            q.pop();
-            result.push_back(f);
-            for(auto &it : mp[f]) {
-                indegree[it]--;
-                if(indegree[it] == 0) {
-                    q.push(it);
-                    count++;
-                }
-            }
-        }
-        if(count == n) return result;
-        return {};
+        st.push(node);
+        inrecursion[node] = false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map <int, vector<int>> mp;
-        vector <int> indegree(numCourses, 0);
+        vector <bool> visited(numCourses, false);
+        vector <bool> inrecursion(numCourses, false);
+        iscycledfs = false;
         for(vector <int>& v : prerequisites) {
             int a = v[0];
             int b = v[1];
             mp[b].push_back(a);
-            indegree[a]++;
         }
-        return topo(mp, numCourses, indegree);
+        stack <int> st;
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) dfs(mp, i, visited, inrecursion, st);
+        }
+        vector <int> ans;
+        if(iscycledfs) return {};
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
     }
 };
