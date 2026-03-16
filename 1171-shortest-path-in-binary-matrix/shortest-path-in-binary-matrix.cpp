ca4de8ee32ea0ae0c5d2149417da1ddbd5c91c5c
Dhruv Totala <dhruvtotla28@gmail.com>
2026-03-17 01:05:39 +0530

    Time: 42 ms (18.96%) | Memory: 23.7 MB (73.20%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> p;
     vector<vector<int>> directions{{1,1}, {0,1}, {-1,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
@@ -9,30 +10,27 @@ public:
         auto issafe = [&](int x, int y) {
             return x >= 0 && x < m && y >= 0 && y < n;
         };
-
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        int level = 0;
-        while(!q.empty()) {
-            int kk = q.size();
-            while(kk--) {
-            auto curr = q.front();
-            q.pop();
-            int x = curr.first;
-            int y = curr.second;
-            if(x == m - 1 && y == n - 1) return level + 1;
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        priority_queue <p, vector <p>, greater <p>> pq;
+        pq.push({0, {0, 0}});
+        result[0][0] = 0;
+        while(!pq.empty()) {
+            int dist = pq.top().first;
+            pair <int, int> node = pq.top().second;
+            int x = node.first;
+            int y = node.second;
+            pq.pop();
             for(auto &dir : directions) {
                 int x_ = x + dir[0];
                 int y_ = y + dir[1];
-                if(issafe(x_, y_) && grid[x_][y_] == 0) {
-                    q.push({x_, y_});
-                    grid[x_][y_] = 1;
+                int wt = 1;
+                if(issafe(x_, y_) && grid[x_][y_] == 0 && dist + wt < result[x_][y_]) {
+                    pq.push({dist + wt, {x_, y_}});
+                    result[x_][y_] = dist + wt;
                 }
             }
         }
-            level++;
-        }
-        return -1;
+        if(result[m - 1][n - 1] == INT_MAX) return -1;
+        return result[m - 1][n - 1] + 1;
     }
 };
