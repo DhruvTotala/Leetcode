edd1fc7069d4f63928f2e16f0ee085c417443842
Dhruv Totala <dhruvtotla28@gmail.com>
2026-03-21 11:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (41.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
+    vector<vector<int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
 
-        queue<pair <int, int>> q;
+        queue<pair<int, int>> q;
         int fresh = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(grid[i][j] == 2) q.push({i, j});
                 else if(grid[i][j] == 1) fresh++;
             }
         }
-
-        if(fresh == 0) return 0;
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] ={0, 1, 0, -1};
         int minutes = -1;
+        if(fresh == 0) return 0;
         while(!q.empty()) {
             int size = q.size();
             minutes++;
             for(int i = 0; i < size; i++) {
-            auto [row, col] = q.front();
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
-            for(int k = 0; k < 4; k++) {
-                int nrow = row + delrow[k];
-                int ncol = col + delcol[k];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1) {
-                        grid[nrow][ncol] = 2; // rot it
-                        fresh--;          // one less fresh orange
-                        q.push({nrow, ncol}); // add to queue for next minute
-                    }
+            for(auto &dir : directions) {
+                int mrow = r + dir[0];
+                int ncol = c + dir[1];
+                if(mrow >= 0 && mrow < m && ncol >= 0 && ncol < n && grid[mrow][ncol] == 1) {
+                    fresh--;
+                    grid[mrow][ncol] = 2;
+                    q.push({mrow, ncol});
+                }
             }
         }
         }
-        return (fresh == 0) ? minutes : -1;
+        if(fresh == 0) return minutes;
+        return -1;
     }
 };
