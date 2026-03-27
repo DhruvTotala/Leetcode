86b3cda09ddf5e8e9a75228f3aab73eb2d761138
Dhruv Totala <dhruvtotla28@gmail.com>
2026-03-27 21:29:27 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (49.69%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.cpp
@@ -3,24 +3,20 @@ public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int m = mat.size();
         int n = mat[0].size();
-
         k %= n;
-
+        if(k == 0) return true;
         for(int i = 0; i < m; i++) {
-            vector<int> temp = mat[i];
-
-            if(i % 2 == 0) {
-                // left rotate using begin()
-                rotate(temp.begin(), temp.begin() + k, temp.end());
-            } 
-            else {
-                // right rotate using rbegin()
-                rotate(temp.rbegin(), temp.rbegin() + k, temp.rend());
+            for(int j = 0; j < n; j++) {
+                int curridx = j;
+                int finalidx;
+                if(i % 2 == 0) { // left shift
+                    finalidx = (j + k) % n;
+                } else { // right shift
+                    finalidx = (j - k + n) % n;
+                }
+                if(mat[i][curridx] != mat[i][finalidx]) return false;
             }
-
-            if(temp != mat[i]) return false;
         }
-
         return true;
     }
 };
