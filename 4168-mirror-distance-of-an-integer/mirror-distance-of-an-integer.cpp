bd1652c1d8b14c134c36e0b1d52a2e6683ea0684
Dhruv Totala <dhruvtotla28@gmail.com>
2026-04-19 01:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.41%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -3,7 +3,6 @@ public:
     int mirrorDistance(int n) {
         string rev_n = to_string(n);
         reverse(rev_n.begin(), rev_n.end());
-        int ans = stoi(rev_n);
-        return abs(n - ans);
+        return abs(n - stoi(rev_n));
     }
 };
