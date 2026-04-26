dfa6ee4f4e00cec98294d2054e7acb57b7684450
Dhruv Totala <dhruvtotla28@gmail.com>
2026-04-27 00:27:15 +0530

    Time: 141 ms (33.04%) | Memory: 208.8 MB (19.63%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    bool containsCycle(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+    int m, n;
+    vector<vector<int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    bool detectbfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& visited) {
+        queue<tuple<int, int, int, int>> q;
+        q.push({i, j, -1, -1});
+        visited[i][j] = true;
+        while(!q.empty()) {
+            auto[curr_i, curr_j, prev_i, prev_j] = q.front();
+            q.pop();
+            for(auto &dir : directions) {
+                int new_i = curr_i + dir[0];
+                int new_j = curr_j + dir[1];
 
-        vector<int> dx = {1, -1, 0, 0};
-        vector<int> dy = {0, 0, 1, -1};
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-
-                if(vis[i][j]) continue;
-
-                queue<tuple<int,int,int,int>> q;
-                q.push({i, j, -1, -1});
-                vis[i][j] = true;
-
-                while(!q.empty()) {
-                    auto [x, y, px, py] = q.front();
-                    q.pop();
-
-                    for(int k = 0; k < 4; k++) {
-                        int nx = x + dx[k];
-                        int ny = y + dy[k];
-
-                        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                        if(grid[nx][ny] != grid[x][y]) continue;
-
-                        if(!vis[nx][ny]) {
-                            vis[nx][ny] = true;
-                            q.push({nx, ny, x, y});
-                        }
-                        else if(nx != px || ny != py) {
-                            return true;
-                        }
-                    }
+                if(new_i >= 0 && new_j >= 0 && new_i < m && new_j < n && grid[new_i][new_j] == grid[curr_i][curr_j]) {
+                    if(new_i == prev_i && new_j == prev_j) continue;
+                    if(visited[new_i][new_j]) return true;
+                    visited[new_i][new_j] = true;
+                    q.push({new_i, new_j, curr_i, curr_j});
                 }
             }
         }
+        return false;
+    }
 
+    bool containsCycle(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!visited[i][j] && detectbfs(i, j, grid, visited)) return true;
+            }
+        }
         return false;
     }
 };
