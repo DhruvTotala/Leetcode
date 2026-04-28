da4b80aee25c2f66690384c487c50f4d2a2980d7
Dhruv Totala <dhruvtotla28@gmail.com>
2026-04-28 19:38:58 +0530

    Time: 44 ms (41.98%) | Memory: 90.1 MB (53.91%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> values;
-        for (const auto& row : grid) {
-            for (int val : row) {
-                values.push_back(val);
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector <int> arr;
+        for(int i = 0; i < m; i++) {
+            for(int val : grid[i]) {
+                arr.push_back(val);
             }
         }
-
-        sort(values.begin(), values.end());
-
-        for (int val : values) {
-            if (abs(val - values[0]) % x != 0) {
-                return -1;
-            }
+        int first_rem = arr[0] % x;
+        for(int val : arr) {
+            if(val % x != first_rem) return -1;
         }
 
-        int median = values[values.size() / 2];
-        int operations = 0;
-
-        for (int val : values) {
-            operations += abs(val - median) / x;
+        sort(arr.begin(), arr.end());
+        int median = arr[arr.size() / 2];
+        int count = 0;
+        for(int val : arr) {
+            count += abs(val - median) / x;
         }
-
-        return operations;
+        return count;
     }
 };
