91053dcaa6e9cb8c77ba0930ed0959b8636f6050
Dhruv Totala <dhruvtotla28@gmail.com>
2026-09-06 11:43:27 +0530

    Time: 30 ms (58.87%) | Memory: 44.3 MB (20.60%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        
+        // dp[i][j] = number of ways t[0..j-1] can be formed from s[0..i-1]
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1, 0));
+        
+        // An empty t can be formed from any prefix of s by deleting all characters
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = 1;
+        }
+        
+        // Fill the table
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+};
