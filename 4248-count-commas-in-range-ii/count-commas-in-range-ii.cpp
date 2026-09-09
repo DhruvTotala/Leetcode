0b8c36ad7a796728c218732901ccc39a2995e14f
Dhruv Totala <dhruvtotla28@gmail.com>
2026-09-09 10:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (99.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -3,16 +3,10 @@ public:
 typedef long long ll;
     long long countCommas(long long n) {
         ll ans = 0;
-        ll lower = 1000;
-        int comma = 1;
-        while(lower <= n) {
-            ll upper = lower * 1000 - 1;
-            if(upper > n) upper = n;
-
-            ll count_nos = upper - lower + 1;
-            ans += comma * count_nos;
-            lower *= 1000;
-            comma++;
+        ll start = 1000;
+        while(start <= n) {
+            ans += (n - start + 1);
+            start *= 1000;
         }
         return ans;
     }
