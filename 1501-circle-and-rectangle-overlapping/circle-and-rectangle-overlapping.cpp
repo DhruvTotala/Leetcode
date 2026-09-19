b2c006de263c43fa2c11c3e44815cb65d64bf81d
Dhruv Totala <dhruvtotla28@gmail.com>
2026-09-19 11:53:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (68.85%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1,
-                      int x2, int y2) {
-        long long dist = 0;
-        if (xCenter < x1 || xCenter > x2) {
-            dist += min(pow(x1 - xCenter, 2), pow(x2 - xCenter, 2));
+    bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int xi, yi;
+
+        if(x1 > xCenter) {
+            xi = x1;
+        } else if(x2 < xCenter) {
+            xi = x2;
+        } else {
+            xi = xCenter;
         }
-        if (yCenter < y1 || yCenter > y2) {
-            dist += min(pow(y1 - yCenter, 2), pow(y2 - yCenter, 2));
+
+        if(y1 > yCenter) {
+            yi = y1;
+        } else if(y2 < yCenter) {
+            yi = y2;
+        } else {
+            yi = yCenter;
         }
-        return dist <= radius * radius;
+
+        int d = sqrt((xCenter - xi) * (xCenter - xi) + (yCenter - yi) * (yCenter - yi));
+        return d <= radius;
     }
 };
